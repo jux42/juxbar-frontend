ba29253b120acfea3f2489b40a494ad90c9de89d
jux42 <roujux@gmail.com>
2024-05-10 17:23:31 +0200

    navbar étape 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./core/components/header/header.component";
 import {slideInAnimation} from "./animations";
+import {NavBarComponent} from "./core/components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   animations: [
